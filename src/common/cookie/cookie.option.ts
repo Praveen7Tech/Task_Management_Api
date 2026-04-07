ca0cb82cacfcd66b5baefb856214a50f6fafe64b
Pr4veenn <praveen.tech.js@gmail.com>
2026-04-07 20:55:23 +0530

    cookie option change

--- src/common/cookie/cookie.option.ts
+++ src/common/cookie/cookie.option.ts
@@ -7,7 +7,7 @@ console.log("node env : ", IS_PRODUCTION)
 
 export const COOKIE_OPTIONS : CookieOptions = {
     httpOnly: true,
-    sameSite: IS_PRODUCTION ? "strict" : "lax",
+    sameSite: IS_PRODUCTION ? "none" : "lax",
     secure: IS_PRODUCTION ? true : false,
     path: "/",
     maxAge: 7*24*60*60*1000,
