3a59720a4b5970c04994a3af504787b3be55b345
Pr4veenn <praveen.tech.js@gmail.com>
2026-04-07 17:49:05 +0530

    cookie option update for production

--- src/common/cookie/cookie.option.ts
+++ src/common/cookie/cookie.option.ts
@@ -1,12 +1,13 @@
 import dotenv from "dotenv"
 dotenv.config()
+import { CookieOptions } from "express";
 
 const IS_PRODUCTION = process.env.NODE_ENV === "production" 
 console.log("node env : ", IS_PRODUCTION)
 
-export const COOKIE_OPTIONS = {
+export const COOKIE_OPTIONS : CookieOptions = {
     httpOnly: true,
-    sameSite: IS_PRODUCTION ? "none" : "lax" as const,
+    sameSite: IS_PRODUCTION ? "none" : "lax",
     secure: IS_PRODUCTION ? true : false,
     path: "/",
     maxAge: 7*24*60*60*1000,
